59e721c9d2e35f0b3b4e77572c96d35747e81ccf
kaalm1 <kaalm1@gmail.com>
2017-07-10 11:12:09 -0400

    Joes Code

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -10,11 +10,10 @@ class GuestsController < ApplicationController
   end
 
   def create
-    byebug
     @appearance = Appearance.new
     @guest = Guest.find(appearance_params[:guest_id])
     @guest.appearances << @appearance
-    Episode.find(appearance_params[:episode_id]).guests << @appearance
+    Episode.find(appearance_params[:episode_id]).appearances << @appearance
     @user = User.find(1) #incorrect -- but only through sessions can I figure out the user so for now its this
     @rating = Rating.new
     @appearance.ratings << @rating
@@ -32,6 +31,6 @@ class GuestsController < ApplicationController
 
   private
   def appearance_params
-    params.require(:user).permit(:episode_id,:guest_id)
+    params.require(:appearance).permit(:episode_id,:guest_id)
   end
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,5 +1,6 @@
 class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
+  has_many :ratings
 
 end
